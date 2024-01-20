21bf1818360e3ecdf2a75bec5e8ab7463a5eb13e
Anton Harbers <Anton.Harbers23@gmail.com>
2024-01-20 19:26:03 +0100

    Feat: Implementation of Update and Delete post methods

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -54,13 +54,55 @@ exports.post_post = [
 ];
 
 exports.update_post = [
+  body('title', 'Title must not be empty')
+    .optional()
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('content', 'Content must not be empty')
+    .optional()
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('is_published', 'is_published must be a boolean')
+    .optional({ checkFalsy: true })
+    .isBoolean()
+    .escape(),
   asyncHandler(async (req, res, next) => {
-    res.send(req.body.content);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(404).json({ errors: errors.array() });
+    }
+
+    const post = await Post.findById(req.params.id).exec();
+    if (!post) {
+      return res.status(404).json({ errors: 'Post not found' });
+    }
+
+    let updatedPost = {};
+    if (req.body.title) updatedPost.title = req.body.title;
+    if (req.body.content) updatedPost.content = req.body.content;
+    if (req.body.is_published) updatedPost.is_published = req.body.is_published;
+
+    await Post.findByIdAndUpdate(req.params.id, updatedPost, {
+      new: true,
+    }).exec();
+
+    return res.send('Post updated succesfully');
   }),
 ];
 
 exports.delete_post = [
   asyncHandler(async (req, res, next) => {
-    res.send(req.body.postId);
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ errors: 'Post not found' });
+    }
+
+    await Post.findByIdAndDelete(req.params.id).exec();
+
+    return res.send('Post deleted');
   }),
 ];
--- routes/post_router.js
+++ routes/post_router.js
@@ -9,6 +9,11 @@ router.get('/', postController.get_posts);
 router.post('/', authenticateJWT, authorizeAdmin, postController.post_post);
 router.get('/:id', postController.get_post);
 router.put('/:id', authenticateJWT, authorizeAdmin, postController.update_post);
-router.get('/:id', authenticateJWT, authorizeAdmin, postController.delete_post);
+router.delete(
+  '/:id',
+  authenticateJWT,
+  authorizeAdmin,
+  postController.delete_post
+);
 
 module.exports = router;
