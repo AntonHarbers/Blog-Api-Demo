9ba0205a001c1e351a9dca900cc059213cbcf7df
Anton Harbers <Anton.Harbers23@gmail.com>
2024-01-21 22:49:49 +0100

    Feat: Implemented put and delete route controllers for comments

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -10,6 +10,7 @@ exports.get_comments = [
     const comments = await Comment.find({})
       .sort({ created_at: 1 })
       .populate('author', 'email')
+      .populate('post', 'title created_at')
       .exec();
 
     res.json(comments);
@@ -18,15 +19,12 @@ exports.get_comments = [
 
 exports.get_comment = [
   asyncHandler(async (req, res, next) => {
-    const comment = await Comment.findById(req.params.id).exec();
-
-    if (!comment) {
-      const err = new Error('Comment not found');
-      err.status = 404;
-      return next(err);
+    try {
+      const comment = await Comment.findById(req.params.id).exec();
+      res.json(comment);
+    } catch (err) {
+      return next('Comment not found');
     }
-
-    res.json(comment);
   }),
 ];
 
@@ -44,7 +42,7 @@ exports.post_comment = [
     .isLength({ min: 1 })
     .escape(),
   asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.body.auhtorId).exec();
+    const user = await User.findById(req.body.authorId).exec();
     const post = await Post.findById(req.body.postId).exec();
 
     if (!user || !post) {
@@ -55,23 +53,61 @@ exports.post_comment = [
 
     const comment = new Comment({
       content: req.body.content,
-      author: req.body.auhtorId,
+      author: req.body.authorId,
       post: req.body.postId,
     });
 
-    const savedPost = await comment.save();
-    return res.json(savedPost);
+    const savedComment = await comment.save();
+    return res.json(savedComment);
   }),
 ];
 
 exports.update_comment = [
+  body('content', 'Content must not be empty')
+    .optional()
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('auhorId', 'Author ID must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('postId', 'Post ID must not be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
   asyncHandler(async (req, res, next) => {
-    res.send(req.body.author);
+    const comment = await Comment.findById(req.params.id).exec();
+
+    if (!comment) {
+      const err = new Error('Comment not found');
+      err.status = 404;
+      return next(err);
+    }
+
+    const updatedComment = new Comment({
+      content: req.body.content,
+      author: req.body.auhtorId,
+      post: req.body.postId,
+    });
+
+    const savedComment = await Comment.findByIdAndUpdate(
+      req.params.id,
+      updatedComment,
+      {}
+    );
+
+    return res.json(savedComment);
   }),
 ];
 
 exports.delete_comment = [
   asyncHandler(async (req, res, next) => {
-    res.send(req.body.commentId);
+    console.log(req.params.id);
+    try {
+      res.json(await Comment.findByIdAndDelete(req.params.id));
+    } catch (err) {
+      res.json(err);
+    }
   }),
 ];
--- routes/comments_router.js
+++ routes/comments_router.js
@@ -8,6 +8,6 @@ router.get('/', commentController.get_comments);
 router.post('/', authenticateJWT, commentController.post_comment);
 router.get('/:id', commentController.get_comment);
 router.put('/:id', authenticateJWT, commentController.update_comment);
-router.get('/:id', authenticateJWT, commentController.delete_comment);
+router.delete('/:id', authenticateJWT, commentController.delete_comment);
 
 module.exports = router;
